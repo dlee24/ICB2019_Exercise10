6fc24df87b1274f9b2c620ad47c7b490a79f0d21
loganthayer1 <logan.thayer@gmail.com>
2019-12-11 11:37:32 -0500

    Final Version Thayer Lee

--- cancerCellPopulation.R
+++ cancerCellPopulation.R
@@ -5,33 +5,30 @@ N0<-99
 M0<-1
 timesteps <- 1500
 
-totalCells[t]<-Nt[t]+Mt[t]
 Nt<-numeric(length=timesteps)
 Nt[1]<-N0
 Mt<-numeric(length=timesteps)
 Mt[1]<-M0
-totalCells<-numeric(length=timesteps)
-totalCells[1]<-totalCells0
 
 #simulation
-for (t in 1:(timesteps-2)){
-  Mt[t+1] <- Mt[t]+rm*Mt[t]*(1-((Mt[t]+Nt[t])/K))
-  Nt[t+1] <- Nt[t]+rm*Nt[t]*(1-((Mt[t]+Nt[t])/K))
-  totalCells[t]<-Mt[t]+Nt[t]
-  if(totalCells[t]<K){
+for (t in 1:(timesteps-1)){
+  if(t < 350){
     rn=0.1
     rm=0.1
-    totalCells[t]<-Mt[t]+Nt[t]
+    Mt[t+1] <- Mt[t]+rm*Mt[t]*(1-((Mt[t]+Nt[t])/K))
+    Nt[t+1] <- Nt[t]+rn*Nt[t]*(1-((Mt[t]+Nt[t])/K))
+  }else if(t >= 350){
+    rn2=-0.1
+    rm2=0.05
+    Mt[t+1] <- Mt[t]+rm2*Mt[t]*(1-((Mt[t]+Nt[t])/K))
+    Nt[t+1] <- Nt[t]+rn2*Nt[t]*(1-((Mt[t]+Nt[t])/K))
   }
-  else if(totalCells[t]>=K){
-    rn=-0.1
-    rm=0.05
-    totalCells[t]<-Mt[t]+Nt[t]
-  }
-  }
+}
+
 
 #plot simulation
 sim<-data.frame(time=1:length(Mt),N=Nt,N2=Mt)
+
 
 options(scipen=5)
 plot(x=sim$time, y=sim$N, type = "l",
@@ -41,4 +38,9 @@ plot(x=sim$time, y=sim$N, type = "l",
      main="Cell Growth Simulation",
      ylim = c(0, 1.1*K))
 lines(x=sim$time, y=sim$N2, type = "l", col = "red")
-legend(170, 850000, legend=c("Non-mutant", "Mutant"),col=c("dark green", "red"), lty=1:1, cex=0.65)
+abline(v=350, lty=4, lwd=3)
+legend(750, 850000, legend=c("Non-mutant", "Mutant","Day Treatment Begins"),
+       col=c("dark green", "red", "black"),
+       lty=c(1,1,4),
+       lwd=c(1,1,3),
+       cex=0.6)
